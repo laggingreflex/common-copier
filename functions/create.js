bac0c9d48a6251f33c6f959b8c1afbfcfd980ae6
laggingreflex <laggingreflex@gmail.com>
2018-06-27 13:54:00 +0530

    fix: ensure projectFile dir

--- functions/create.js
+++ functions/create.js
@@ -4,8 +4,8 @@ const fs = require('fs-extra');
 module.exports = (files, { commonDir, projectDir }) => Promise.all(files.map(async (file) => {
   const commonFile = Path.join(commonDir, file);
   const projectFile = Path.join(projectDir, file);
-  await fs.ensureDir(projectDir);
-  // console.log(file);
+  await fs.ensureDir(Path.dirname(projectFile));
+  // console.log({file, projectDir});
   await fs.remove(projectFile);
   await fs.link(commonFile, projectFile);
 }));
